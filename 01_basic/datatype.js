bf60b6b78ee2f6cca7d31d0f2c95b47dc9df7f43
Akanksha Dixit <2k23.cscys2310306@gmail.com>
2025-08-04 22:42:40 +0000

    4

---
+++ 01_basic/datatype.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all the js code as newer version
+
+// alert(3+3) we are using nodejs not browser , so this will not work here
+
+//tc39 and mdn for documentation (echma script)
+
+/* number 
+bigint
+string=""
+boolean=true/false
+null= standalone value
+undefined= not assigned any value
+symbol
+(these are primitive values)
+
+//object
+
+*/
+//typeof
+console.log(typeof "789");
+console.log(typeof null);     //output=object
+console.log(typeof undefined);        //output=undefined
+
+
+
+
