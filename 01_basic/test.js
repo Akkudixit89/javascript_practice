f1ff7a82af2bd0a1865c206c1ddf706e9cd49227
Akanksha Dixit <2k23.cscys2310306@gmail.com>
2025-08-04 22:20:12 +0000

    3

--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,3 +1,15 @@
 console.log("akanksha");
 console.log("akku");
 console.log("hello");
+
+const first=345
+let second="hello let"
+var third="byy var i will not use you"
+fourth="i am without any keyword";
+let fifth;
+
+// not allowed to modify const variable first=89
+// don't use var because of issue in block scope and functional scope
+
+console.log(first);
+console.table([first,second,third,fourth,fifth])               //shortcut for many printing variables
